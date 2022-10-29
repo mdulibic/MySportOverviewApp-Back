00e4d06e3dcf5ffb907f26848d211f3d4edba697
Marta <dulibicmarta00@gmail.com>
2022-10-29 14:32:00 +0200

    inital setup

---
+++ src/main/java/com/example/mysportoverviewapp/MySportOverviewAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysportoverviewapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySportOverviewAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySportOverviewAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mysportoverviewapp/MySportOverviewAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysportoverviewapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySportOverviewAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
