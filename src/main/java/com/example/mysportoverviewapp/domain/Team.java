39732c7a145c3086f19074aa74464b423b18cbce
Marta <dulibicmarta00@gmail.com>
2022-10-30 13:09:01 +0100

    add getters and setters for points and goals in team

--- src/main/java/com/example/mysportoverviewapp/domain/Team.java
+++ src/main/java/com/example/mysportoverviewapp/domain/Team.java
@@ -33,7 +33,23 @@ public class Team {
     public void setName(String name) {
         this.name = name;
     }
-    
+
+    public Integer getPoints() {
+        return points;
+    }
+
+    public void setPoints(Integer points) {
+        this.points = points;
+    }
+
+    public Integer getGoals() {
+        return goals;
+    }
+
+    public void setGoals(Integer goals) {
+        this.goals = goals;
+    }
+
     public Team() {
 
     }
